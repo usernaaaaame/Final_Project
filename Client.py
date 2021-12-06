34b109721774338b6d7d846c934c01e389d99ec2
예시 <takey417@gmail.com>
2021-12-06 11:13:39 +0900

    첫번째 코딩

--- Client.py
+++ Client.py
@@ -13,9 +13,9 @@ class Client ():
         self.Main_Page()
 
     def Main_Page(self):
-        id_to_task = int(input('하고 싶은 작업을 선택하여 숫자로 입력해 주세요 : 0-종료, 1-사역 확인, 2-사역 면제 쿠폰 사용\n'))
+        id_to_task = int(input('하고 싶은 작업을 선택하여 숫자로 입력해 주세요 : 0-종료, 1-예정 사역 확인, 2-사역 면제 쿠폰 사용\n'))
         while (id_to_task!=0 and id_to_task!=1 and id_to_task!=2):
-            id_to_task = int(input('입력 에러! 숫자를 1 또는 2로 입력해 주세요 : 0-종료, 1-사역 확인, 2-사역 면제 쿠폰 사용\n'))
+            id_to_task = int(input('입력 에러! 숫자를 1 또는 2로 입력해 주세요 : 0-종료, 1-예정 사역 확인, 2-사역 면제 쿠폰 사용\n'))
         if id_to_task ==1 :
             self.Confirm()
         elif id_to_task==2 :
@@ -37,7 +37,7 @@ class Client ():
         id_to_search = int(input('확인 하고 싶은 사역의 번호를 적어주세요 : '))
         while (id_to_search<1 or id_to_search>len_wsts):
             id_to_search = int(input('입력 에러! 범위에 맞는 숫자 값을 입력해주세요 : '))
-        print(pd.read_excel('WorkCycle.xlsx',sheet_name=str(self.ws_names_to_show[id_to_search-1])))
+        print(pd.read_excel('WorkCycle.xlsx',sheet_name=str(self.ws_names_to_show[id_to_search-1]),usecols=[0,2]))
         self.Main_Page()
 
     def Request(self):
